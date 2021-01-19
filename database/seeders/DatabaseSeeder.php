55f3b13e09c0991cb041016403e8871e59d5537a
YuSan357 <forestgreen14689@gmail.com>
2021-01-19 17:50:44 +0900

    game_record表初期設定用のSeederを作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,9 @@ class DatabaseSeeder extends Seeder
   public function run()
   {
     // \App\Models\User::factory(10)->create();
+    $this -> call([
+      GameRecordTableSeeder::class
+    ]);
     
     
   }
