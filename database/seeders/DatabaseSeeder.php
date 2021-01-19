ba7a0c60942e981073140ff829662f94a8ccd116
YuSan357 <forestgreen14689@gmail.com>
2021-01-19 16:41:06 +0900

    migrationやdockerやらいろいろ試した

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,43 @@
 
 namespace Database\Seeders;
 
+use App\Models\GameRecord;
 use Illuminate\Database\Seeder;
+// use Log;
+use DB;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    // \App\Models\User::factory(10)->create();
+    DB::transaction(function(){
+      GameRecord::create([
+        'stage_name' => 'Level1',
+      ]);
+      GameRecord::create([
+        'stage_name' => 'Level2',
+      ]);
+      GameRecord::create([
+        'stage_name' => 'Level3',
+      ]);
+      GameRecord::create([
+        'stage_name' => 'Level4',
+      ]);
+      GameRecord::create([
+        'stage_name' => 'Score Attack',
+      ]);
+      GameRecord::create([
+        'stage_name' => 'Survival',
+      ]);
+  
+      // Log::debug(GameRecord::count());
+    });
+    
+  }
 }
