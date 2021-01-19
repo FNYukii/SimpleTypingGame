a29d3e6411fea685d426e7871b51541a3633649c
YuSan357 <forestgreen14689@gmail.com>
2021-01-19 17:26:21 +0900

    色々触っていたのでもとの状態に戻した

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,28 +17,7 @@ class DatabaseSeeder extends Seeder
   public function run()
   {
     // \App\Models\User::factory(10)->create();
-    DB::transaction(function(){
-      GameRecord::create([
-        'stage_name' => 'Level1',
-      ]);
-      GameRecord::create([
-        'stage_name' => 'Level2',
-      ]);
-      GameRecord::create([
-        'stage_name' => 'Level3',
-      ]);
-      GameRecord::create([
-        'stage_name' => 'Level4',
-      ]);
-      GameRecord::create([
-        'stage_name' => 'Score Attack',
-      ]);
-      GameRecord::create([
-        'stage_name' => 'Survival',
-      ]);
-  
-      // Log::debug(GameRecord::count());
-    });
+    
     
   }
 }
