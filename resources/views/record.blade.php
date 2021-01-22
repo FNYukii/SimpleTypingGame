677a5eb2af5f2b43744525d5c1503c44b7945083
YuSan357 <forestgreen14689@gmail.com>
2021-01-22 22:12:34 +0900

    新しいページのレイアウトを決めた。 playedTimeとTypedCount両方を記録することにした

--- resources/views/record.blade.php
+++ resources/views/record.blade.php
@@ -18,30 +18,37 @@ Record
       <tr>
         <th>ステージ名</th>
         <th>プレイ時間</th>
+        <th>入力した単語数</th>
       </tr>
       <tr>
         <td class="stage-name nomalmode-color">LEVEL 1</td>
         <td><span id="record_level1Span">xx.xx秒</span></td>
+        <td><span id="">xx 個</span></td>
       </tr>
       <tr>
         <td class="stage-name nomalmode-color">LEVEL 2</td>
-        <td><span id="record_level2Span">xx.xx秒</span></td>
+        <td><span id="record_level2Span">xx.xx 秒</span></td>
+        <td><span id="">xx 個</span></td>
       </tr>
       <tr>
         <td class="stage-name nomalmode-color">LEVEL 3</td>
-        <td><span id="record_level3Span">xx.xx秒</span></td>
+        <td><span id="record_level3Span">xx.xx 秒</span></td>
+        <td><span id="">xx 個</span></td>
       </tr>
       <tr>
         <td class="stage-name nomalmode-color">LEVEL 4</td>
-        <td><span id="record_level4Span">xx.xx秒</span></td>
+        <td><span id="record_level4Span">xx.xx 秒</span></td>
+        <td><span id="">xx 個</span></td>
       </tr>
       <tr>
         <td class="stage-name score-attack-color">Score Attack</td>
-        <td><span id="record_scoreAttackSpan">xx個</span></td>
+        <td><span id="">xx.xx 秒</span></td>
+        <td><span id="record_scoreAttackSpan">xx 個</span></td>
       </tr>
       <tr>
         <td class="stage-name survival-color">Survival</td>
-        <td><span id="record_survivalSpan">xx.xx秒</span></td>
+        <td><span id="record_survivalSpan">xx.xx 秒</span></td>
+        <td><span id="">xx 個</span></td>
       </tr>
     </table>
   </div>
